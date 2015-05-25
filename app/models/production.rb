1548d27d58411ebbea4d2e96a5838d8df97de248
Majd Murad <murad.majd@gmail.com>
2015-05-24 23:46:13 -0700

    Basically finished the app. Needs validations and rspec tests

--- app/models/production.rb
+++ app/models/production.rb
@@ -7,4 +7,12 @@ class Production < ActiveRecord::Base
   def rating
     (votes.map(&:score).inject(:+)/votes.count.to_f).round(2)
   end
+
+  def dates
+    perf_dates = []
+    performances.each do |perf|
+      perf_dates << perf.date_and_time
+    end
+    perf_dates
+  end
 end
